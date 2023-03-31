b569649642c930063bc89b83207a7efe56cd54b5
jorgetomtz <88950055+jorgetomtz@users.noreply.github.com>
2023-03-31 12:49:47 -0400

    Fix check_version_bumped and add unit tests (#18)

--- test/test_check_version_bumped.py
+++ test/test_check_version_bumped.py
@@ -1,5 +1,35 @@
 from __future__ import annotations
 
+from custom_hooks import check_version_bumped
 
-def test_add():
-    assert 1 + 1 == 2
+
+def test_no_bump(tmpdir, fake_git_no_changes):
+    d = tmpdir / "d"
+    d.mkdir()
+    f = d / "setup.cfg"
+    f.write("version = 0.1.0")
+    f1 = d / "a.py"
+    f1.write("hello")
+    assert check_version_bumped.main([f"{f1}"]) == 0
+
+
+def test_no_bump_and_required(capsys, tmpdir):
+    d = tmpdir / "d"
+    d.mkdir()
+    f = d / "pyproject.toml"
+    f.write("version = 0.1.0")
+    f1 = d / "a.py"
+    f1.write("hello")
+    check_version_bumped.main([f"{f1}"])
+    cap = capsys.readouterr()
+    assert f"Version bumped required in {f}" in cap.out
+
+
+def test_bump_and_required(capsys, tmpdir, fake_git_version_changes):
+    d = tmpdir / "d"
+    d.mkdir()
+    f = d / "pyproject.toml"
+    f.write("version = 0.1.0")
+    f1 = d / "a.py"
+    f1.write("hello")
+    assert check_version_bumped.main([f"{f1}"]) == 0

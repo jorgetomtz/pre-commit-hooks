ec118d0d729c15be40f22ac0fce676b86f813e41
Jorge Torres Martinez <jorgetomtz@gmail.com>
2023-04-06 13:50:50 -0400

    f

--- test/test_copyright_checker.py
+++ test/test_copyright_checker.py
@@ -250,34 +250,34 @@ def test_chained_licenses_in_java_current_copyright(capsys, tmpdir):
     year = str(datetime.date.today().year)
     f.write(
         f"""
-        /*! ****************************************************************************
-         *
-         * Other company
-         *
-         * Copyright (C) 2002-2017 by other
-         *
-         *******************************************************************************
-         *
-         * Licensed under the Apache License, Version 2.0 (the "License");
-         * you may not use this file except in compliance with
-         * the License. You may obtain a copy of the License at
-         *
-         *    http://www.apache.org/licenses/LICENSE-2.0
-         *
-         * Unless required by applicable law or agreed to in writing, software
-         * distributed under the License is distributed on an "AS IS" BASIS,
-         * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-         * See the License for the specific language governing permissions and
-         * limitations under the License.
-         *
-         ******************************************************************************/
+/*! ****************************************************************************
+ *
+ * Other company
+ *
+ * Copyright (C) 2002-2017 by other
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 
-        /**
-         * Copyright (c) 2016, {year} by fake. All rights reserved.
-         */
-        package something;
+/**
+ * Copyright (c) 2016, {year} by fake. All rights reserved.
+ */
+package something;
 
-        import java.io.something;
+import java.io.something;
         """
     )
     assert copyright_checker.main(["-o", "fake", f"{f}"]) == 0

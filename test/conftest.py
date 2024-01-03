cfce6d28bdba0aefcb6ee1d5c332806d6acdf5e1
Jules Tamagnan <jules.tamagnan@delphix.com>
2024-01-03 15:52:20 -0800

    copyright-checker should look at the most recent git timestamp

--- test/conftest.py
+++ test/conftest.py
@@ -2,34 +2,52 @@ from __future__ import annotations
 
 import pytest
 
-CHANGES = """
-+Changes Found
-"""
 
+class FakeDiff:
+    def __init__(self, a_path):
+        self.a_path = a_path
+
+class FakeIndex:
+    def __init__(self, diffs):
+        self.diffs = diffs
+
+    def diff(self, *args, **kwargs):
+        return self.diffs
 
 class FakeGit:
-    def __init__(self, changes):
+    def __init__(self, changes, date):
         self.changes = changes
+        self.date = date
 
-    def diff(self, args):
+    def diff(self, *args, **kwargs):
         return self.changes
+
+    def log(self, *args, **kwargs):
+        return self.date
 
 
 class FakeGitRepo:
-    def __init__(self, changes):
-        self.git = FakeGit(changes)
+    def __init__(self, changes, date, working_dir, files):
+        self.git = FakeGit(changes, date)
+        self.index = FakeIndex([FakeDiff(f) for f in files])
+        self.working_dir = working_dir
+
+
+@pytest.fixture()
+def date():
+    return "1704321349"
+
+
+@pytest.fixture()
+def changes():
+    return "\n+Changes Found\n"
+
+
+@pytest.fixture()
+def files():
+    return ["CHANGELOG.md"]
 
 
 @pytest.fixture(autouse=True)
-def fake_git(mocker):
-    mocker.patch("git.Repo", return_value=FakeGitRepo(CHANGES))
-
-
-@pytest.fixture
-def fake_git_no_changes(mocker):
-    mocker.patch("git.Repo", return_value=FakeGitRepo(""))
-
-
-@pytest.fixture
-def fake_git_version_changes(mocker):
-    mocker.patch("git.Repo", return_value=FakeGitRepo("+version = 0.1.0"))
+def fake_git(mocker, date, changes, files, tmpdir):
+    mocker.patch("git.Repo", return_value=FakeGitRepo(changes, date, tmpdir, files))
